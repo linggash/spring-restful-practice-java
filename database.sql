0f9ce8b169e61a6f395d0f82cd822f74f567bfe1
linggash <sempanalingga@gmail.com>
2023-06-19 19:52:11 +0700

    user and contact entity was added

---
+++ database.sql
@@ -0,0 +1,22 @@
+USE spring_restful_practice
+
+CREATE TABLE users (
+    username            VARCHAR(100) NOT NULL,
+    password            VARCHAR(100) NOT NULL,
+    name                VARCHAR(100) NOT NULL,
+    token               VARCHAR(100),
+    token_expired_at    BIGINT,
+    PRIMARY KEY (username),
+    UNIQUE (token)
+) ENGINE InnoDB;
+
+CREATE TABLE contacts(
+    id          VARCHAR(100) NOT NULL,
+    username    VARCHAR(100) NOT NULL,
+    first_name  VARCHAR(100) NOT NULL,
+    last_name   VARCHAR(100),
+    phone       VARCHAR(100),
+    email       VARCHAR(100),
+    PRIMARY KEY (id),
+    FOREIGN KEY fk_users_contacts (username) REFERENCES users(username)
+) ENGINE InnoDB;
